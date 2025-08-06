0100b3ec701118cbcef0808e10ff9b14c631dffa
rajuratjod <rajurathod.11301@gmail.com>
2025-08-06 01:00:09 -0700

    Bind to Render's PORT env variable

--- backend/RestApiServer.java
+++ backend/RestApiServer.java
@@ -20,7 +20,7 @@ public class RestApiServer {
     private static final String DB_PASSWORD = "raju"; // Update with your MySQL password
     
     public static void main(String[] args) throws IOException {
-       HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 0000), 0);
+       HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
 
 
         
